16440473b76954c1b34d5f83f9581a90af7c68c9
sk.ahm <itzshakeey@gmail.com>
2023-10-11 19:53:20 +0100

    minor update on colour codes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,14 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-yellow-300' />
+          <div className='w-1 sm:h-80 h-40 yellow-gradient' />
         </div>
+
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#f8f525]'>Sheikh Khaled</span>
+            Hi, I'm <span className='text-[#FFD700]'>Sheikh Khaled</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Cloud Engineer,  <br className='sm:block hidden' />
